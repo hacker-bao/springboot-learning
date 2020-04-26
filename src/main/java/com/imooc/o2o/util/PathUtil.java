7bdd852618de3f9327fd250a3ee9fbbc188cd1ac
hacker-bao <39732003+hacker-bao@users.noreply.github.com>
2020-04-26 09:26:42 +0800

    缩略图处理

---
+++ src/main/java/com/imooc/o2o/util/PathUtil.java
@@ -0,0 +1,24 @@
+package com.imooc.o2o.util;
+
+public class PathUtil {
+
+    private static String separator = System.getProperty("file.separator");
+
+    public static String getImgBasePath(){
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if(os.toLowerCase().startsWith("win")){
+            basePath = "D:/upload/images";
+        }else{
+            basePath = "/home/xiangze/image/";
+        }
+
+        basePath = basePath.replace("/", separator);
+        return basePath;
+    }
+
+    public static String getShopImagePath(long shopId){
+        String imagePath = "/item/shop/" + shopId + "/";
+        return imagePath.replace("/", separator);
+    }
+}
