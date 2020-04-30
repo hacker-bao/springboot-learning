5baac19a0956f26c83d2d619252ab8fbfb221455
hacker-bao <39732003+hacker-bao@users.noreply.github.com>
2020-04-30 19:32:50 +0800

    店铺添加

--- src/main/java/com/imooc/o2o/util/ImageUtil.java
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -87,7 +87,7 @@ public class ImageUtil {
      * 生成随机文件名，当前年月日小时分钟秒钟+五位随机数
      * @return
      */
-    private static String getRandomFileName() {
+    public static String getRandomFileName() {
         int rannum = r.nextInt(89999) + 10000;
         String nowTimeStr = sDateFormat.format(new Date());
         return nowTimeStr + rannum;
